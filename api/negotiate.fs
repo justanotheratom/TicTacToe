9ffa814dcb0230f3df3798abdfe84fc2cfe05db5
justanotheratom <sanketpatel.1805@gmail.com>
2022-09-11 21:41:20 +0000

    Remove UserId from SignalRConnectionInfo binding

--- api/negotiate.fs
+++ api/negotiate.fs
@@ -13,7 +13,7 @@ open Microsoft.Azure.WebJobs.Extensions.SignalRService
 [<FunctionName("negotiate")>]
 let negotiate
     ([<HttpTrigger(AuthorizationLevel.Anonymous)>] req: HttpRequest)
-    ([<SignalRConnectionInfo (HubName = "tictactoe", UserId = "{headers.x-ms-client-principal-id}")>] connectionInfo: SignalRConnectionInfo)
+    ([<SignalRConnectionInfo (HubName = "tictactoe")>] connectionInfo: SignalRConnectionInfo)
     :
     SignalRConnectionInfo
     =
